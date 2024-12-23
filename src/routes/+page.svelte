4f2032034c722afcb453d080fafe94a8f36387e4
dmihovch <dmihovch@gmail.com>
2024-12-23 13:04:46 -0500

    make and centered header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,10 @@
 
 
 
-
+<div id="header">
+	<h1>Dan Mihovch</h1>
+	<h2>Computer Science @ University of Delaware</h2>
+</div>
 <p>
 	My Github <a href="https://github.com/dmihovch" target="_blank" rel="noopener noreferrer"
 		>github.com/dmihovch</a
@@ -14,7 +17,19 @@
 		>linkedin.com/in/danielmihovch
 	</a>
 </p>
-
-
 <a href="/projects">My projects</a>
 <a href="/resume">My resume</a>
+
+<style>
+	#header {
+		display: flexbox;
+		justify-content: center;
+		align-items:center;
+		text-align: center;
+		position: absolute;
+		top: 35%;
+		left: 50%;
+		transform: translate(-50%,-50%);
+		
+	}
+</style>
