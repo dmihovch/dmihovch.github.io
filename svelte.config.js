bad228621b9f797ddab0031c8720ddda4aaa8d5b
dmihovch <dmihovch@gmail.com>
2024-12-26 19:03:27 -0500

    vercel config 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,7 @@
 import adapter from '@sveltejs/adapter-vercel';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter(
-			{
-				runtime: 'nodejs18.x',
-			}
-		),
-	},
+ 
+export default {
+  kit: {
+    adapter: adapter(),
+  },
 };
-export default config;
