6cf0fe2c75b188dd5c924cbbed58231d7ef76033
Daniel Mihovch <dmihovch@gmail.com>
2026-03-30 12:44:54 -0400

    started parser

--- src/scripts/shell.ts
+++ src/scripts/shell.ts
@@ -1,6 +1,45 @@
 
+export type CmdType = 'input' | 'output' | 'noop' ;
 
-function parseInput(raw_cmd: string)
-{
-  
+export type CmdHistory = {
+  type: CmdType;
+  text: string;
+  return_code: number;
 }
+
+export function parseInput(raw_cmd:string): CmdHistory
+{
+    let tokens:string[] = tokenizeInput(raw_cmd);
+    if(tokens.length === 0)
+    {
+      return {type: 'noop', text: '', return_code: 0};
+    }
+
+}
+
+function tokenizeInput(raw_cmd: string): string[]
+{
+  return raw_cmd.split(' ');
+}
+
+function runCmd(tokens:string[]): number
+{
+  if(tokens[0] === 'cd')
+  {
+    return cmd_cd(tokens);
+  }
+  if(tokens[0] === 'ls')
+  {
+    return cmd_ls(tokens);
+  }
+}
+
+function cmd_cd(args: string[]): number
+{
+  return 0;
+}
+function cmd_ls(args: string[]): number
+{
+  return 0;
+}
+
